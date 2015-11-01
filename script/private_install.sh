dbc955f25e6c0d8149c8744a7dae7ace9b61e2ff
pidipidi <deric.park@gatech.edu>
2015-11-01 03:35:12 -0500

    modified:   script/private_install.sh 	modified:   util/bashrc_14_04

--- script/private_install.sh
+++ script/private_install.sh
@@ -1,7 +1,7 @@
 sudo apt-get install meld cifs-utils vpnc 
 sudo easy_install pip
 
-sudo apt-get install ros-indigo-urdfdom-py ros-indigo-ar-track-alvar ros-indigo-pr2-msgs ros-indigo-pr2-controllers-msgs
+sudo apt-get install ros-indigo-urdfdom-py ros-indigo-ar-track-alvar ros-indigo-pr2-msgs ros-indigo-pr2-controllers-msgs ros-indigo-pr2-kinematics python-openopt ros-indigo-sound-play
 
 ## git (After adding key)
 #cd ~/git
@@ -12,6 +12,7 @@ git clone https://github.com/gt-ros-pkg/hrl-lib.git
 git clone https://github.com/gt-ros-pkg/hrl-kdl.git
 git clone https://github.com/gt-ros-pkg/hrl-sensor-utils.git
 git clone https://github.com/gt-ros-pkg/hrl_autobed_dev.git
+git clone https://github.com/gt-ros-pkg/hrl.git
 
 cd ~/catkin_ws/src/hrl-haptic-manip-dev/
 git checkout indigo-devel
@@ -21,6 +22,8 @@ cd ~/catkin_ws/src/hrl-sensor-utils/
 git checkout hydro-devel
 cd ~/catkin_ws/src/hrl-assistive/
 git checkout indigo-devel
+cd ~/catkin_ws/src/hrl/
+git checkout hydro-devel
 
 # compile
 cd ~/catkin_ws/
@@ -28,7 +31,7 @@ catkin_make_isolated --only-pkg-with-deps hrl_kdl --merge
 catkin_make_isolated --only-pkg-with-deps hrl_haptic_mpc --merge
 catkin_make_isolated --only-pkg-with-deps sandbox_dpark_darpa_m3 --merge
 catkin_make_isolated --only-pkg-with-deps hrl_anomaly_detection --merge
-
+catkin_make_isolated --only-pkg-with-deps equilibrium_point_control --merge
 
 ## machine learning
 # Scipy
