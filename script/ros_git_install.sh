e07c6d7e0a29c109573f04a6ecf56d10e1d5d098
pidipidi <deric.park@gatech.edu>
2016-07-02 15:43:05 -0400

    Updated installation scripts

--- script/ros_git_install.sh
+++ script/ros_git_install.sh
@@ -10,6 +10,7 @@ git clone https://github.com/gt-ros-pkg/hrl-kdl.git
 git clone https://github.com/gt-ros-pkg/hrl-sensor-utils.git
 git clone https://github.com/gt-ros-pkg/hrl_autobed_dev.git
 git clone https://github.com/gt-ros-pkg/hrl.git
+git clone https://github.com/gt-ros-pkg/hrl-pr2-behaviors.git
 
 cd ~/catkin_ws/src/hrl-haptic-manip-dev/
 git checkout indigo-devel
@@ -21,21 +22,22 @@ cd ~/catkin_ws/src/hrl-assistive/
 git checkout indigo-devel
 cd ~/catkin_ws/src/hrl/
 git checkout indigo-devel
+cd ~/catkin_ws/src/hrl-kdl/
+git checkout dpark_test
+cd ~/catkin_ws/src/hrl-pr2-behaviors/
+git checkout indigo-devel
+
 
 # compile
 cd ~/catkin_ws/
 catkin_init_workspace
+catkin_make_isolated --only-pkg-with-deps hrl_srvs --merge
 catkin_make_isolated --only-pkg-with-deps hrl_kdl --merge
 catkin_make_isolated --only-pkg-with-deps hrl_haptic_mpc --merge
 catkin_make_isolated --only-pkg-with-deps sandbox_dpark_darpa_m3 --merge
 catkin_make_isolated --only-pkg-with-deps hrl_anomaly_detection --merge
 catkin_make_isolated --only-pkg-with-deps equilibrium_point_control --merge
+catkin_make_isolated --only-pkg-with-deps hrl_realsense_camera --merge
 
 
-
-## Sensor
-#sudo apt-get install ros-indigo-rtt ros-indigo-rtt-ros ros-indigo-rtt-rosclock ros-hydro-force-torque-sensor-controller ros-hydro-controller-manager 
-#cd ~/catkin_ws/src/
-#git clone git clone https://github.com/ros-drivers/ati_netft_ros_driver.git
-#catkin_make_isolated --only-pkg-with-deps netft_rdt_driver --merge
 
