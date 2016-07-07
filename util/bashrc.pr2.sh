0857ad31bb3b6158f7e331acb7094646fa7f033d
pidipidi <deric.park@gatech.edu>
2016-07-07 17:08:59 -0400

    modified:   util/bashrc.pr2.sh modified:   util/bashrc_14_04

--- util/bashrc.pr2.sh
+++ util/bashrc.pr2.sh
@@ -1,5 +1,6 @@
+export ROS_ENV_LOADER=/etc/ros/env.sh
+export KINECT2=true
 
 alias dash='rosrun pr2_dashboard pr2_dashboard'
 alias drive='roslaunch pr2_teleop teleop_joystick.launch'
 alias kinect='ssh administrator@10.68.0.103'
-export KINECT2=true
